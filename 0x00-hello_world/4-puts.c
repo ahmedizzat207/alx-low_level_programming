3896d3ab9cae47e3d934611af97663e7e882f212
Ahmed Izzat <ahmedizzat207@gmail.com>
2023-12-14 03:54:35 -0800

    Task4:Change: write documentation and correct the text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - print the string to standard output
+ *
+ * Return: always (0)
+ */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
