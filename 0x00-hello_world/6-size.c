c53380d5e0e25e3ddf67d545bf33ab80a81bf316
Ahmed Izzat <ahmedizzat207@gmail.com>
2023-12-14 04:17:02 -0800

    Task6:Add: 6-size.c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the size of various types in the computer
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of char: %d byte(s)\n", sizeof(c));
+	printf("Size of int: %d byte(s)\n", sizeof(i));
+	printf("Size of long int: %d byte(s)\n", sizeof(li));
+	printf("Size of long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of float: %d byte(s)\n", sizeof(f));
+
+	return (0);
+}
