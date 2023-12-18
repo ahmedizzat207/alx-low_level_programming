7de898872fd3e9ba0cd4e360e1a77f13396faab5
Ahmed Izzat <ahmedizzat207@gmail.com>
2023-12-18 07:23:39 -0800

    Task11:Add: 101-print_comb4.c file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible different combination of three digits
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	int h;
+	int t;
+	int o;
+
+	h = 48;
+	while (h < 56)
+	{
+		t = h + 1;
+		while (t < 57)
+		{
+			o = t + 1;
+			while (o <= 57)
+			{
+				putchar(h);
+				putchar(t);
+				putchar(o);
+				if (h < 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				o++;
+			}
+			t++;
+		}
+		h++;
+	}
+	putchar('\n');
+	return (0);
+}
