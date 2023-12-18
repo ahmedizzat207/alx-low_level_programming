faa379cd2e4564b92d1ae76f48a2f40e3e662584
Ahmed Izzat <ahmedizzat207@gmail.com>
2023-12-18 05:34:33 -0800

    Task1:Fix: add new line to the last two conditional

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,8 +20,8 @@ int main(void)
 	if (n > 5)
 		printf("greater than 5\n");
 	else if (n == 0)
-		printf("0");
+		printf("0\n");
 	else
-		printf("less than 6 and not 0");
+		printf("less than 6 and not 0\n");
 	return (0);
 }
