9db4d0d6d268d46a71f75ddcaa8e2ec4ee4f1cf4
Ahmed Izzat <ahmedizzat207@gmail.com>
2023-12-18 05:40:21 -0800

    Task1:Fix: do betty istructions

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,8 +4,8 @@
 #include <stdio.h>
 /* betty style doc for function main goes there */
 /**
- * main - identify whether the last digit of a random generated number is greater than 5 or
- * less than 6 and not 0 or is 0
+ * main - identify whether the last digit of a random generated number is
+ * greater than 5 or less than 6 and not 0 or is 0
  *
  * Return: always (0)
  */
@@ -16,10 +16,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	printf("Last digit of %d is %d and is ", n, n%10);
-	if (n%10 > 5)
+	printf("Last digit of %d is %d and is ", n, n % 10);
+	if (n % 10 > 5)
 		printf("greater than 5\n");
-	else if (n%10 == 0)
+	else if (n % 10 == 0)
 		printf("0\n");
 	else
 		printf("less than 6 and not 0\n");
