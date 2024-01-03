7edf0dd7fcf9f1cb2cb73fa32b033f372d787d36
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-01-02 19:05:09 -0800

    Task4:Add: 4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Print a string in reverse
+ *
+ * @s: the string to be printed in reverse
+ */
+
+void print_rev(char *s)
+{
+	int n;
+
+	n = 0;
+	while (*(s + n) != '\0')
+	{
+		n++;
+	}
+	n -= n;
+	while (n >= 0)
+	{
+		putchar(*(s + n));
+		n--;
+	}
+}
