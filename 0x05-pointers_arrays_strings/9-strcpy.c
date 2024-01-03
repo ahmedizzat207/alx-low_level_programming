0c839e64542db99709a87871bb6d1a642082c7a6
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-01-03 08:29:17 -0800

    Task9:Add: 9-strcpy.c file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy a string from src to dest
+ * @dest: the destenation in which to copy the string to
+ * @src: the string to be copyed
+ *
+ * Return: the point to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int counter;
+
+	counter = 0;
+	while (*(src + counter) != '\0')
+	{
+		*(dest + counter) = *(src + counter);
+		counter++;
+		if (*(src + counter) != '\0')
+			*(dest + counter) = *(src + counter);
+	}
+}
