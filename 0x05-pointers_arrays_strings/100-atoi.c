b66afa093a54af6bf00dc25b2cf56057a985a0e0
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-01-03 08:57:16 -0800

    Task10:Add: 100-atoi.c file

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert a string to integer
+ * @s: the string to be converted
+ *
+ * Return: the integer that had been converted
+ */
+
+int _atoi(char *s)
+{
+	int sign;
+	int sum;
+	int counter;
+	int result;
+
+	counter = 0;
+	sign = 1;
+	sum = 0;
+	while (*(s + counter) != '\0')
+	{
+		if (*(s + counter) == '-')
+			sign *= -1;
+		if ((*(s + counter) >= '0') && (*(s + counter) <= '9'))
+			sum *= 10 + *(s + counter);
+		counter++;
+	}
+	result = sign * sum;
+	return (result);
+}
