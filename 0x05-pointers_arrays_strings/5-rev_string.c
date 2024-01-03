8ebcaccf97076e46403cc6c4fe9c16498db7d437
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-01-02 19:18:03 -0800

    Task5:Add: 5-rev_string.c file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - reverse the given string
+ *
+ * @s: the string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int n;
+
+	n = 0;
+	while (*(s + n) != '\0')
+	{
+		n++;
+	}
+	n -= 1;
+	while (n >= 0)
+	{
+		putchar(*(s + n));
+		n--;
+	}
+}
