6533cba34072af1cb75733c1afb70d9c94c872bc
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-01-03 08:14:44 -0800

    Task8:Add: 8-print_array.c file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print n elemants of an array of integers, followed by
+ * a newline
+ *
+ * @a: the array of integers
+ * @n: the number of elements to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int counter;
+
+	counter = 0;
+	while (counter < n)
+	{
+		putchar(*(a + n));
+		counter++;
+	}
+	putchar('\n');
+}
