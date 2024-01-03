a3e8d25a3217c6f6b197678ec31bd33a1a21484c
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-01-02 18:45:06 -0800

    Task2:Add: 2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - count and return the length of a string
+ * @s: the string to be measured
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	n = 0;
+	while (*(s+n) != '\0')
+	{
+		n++;
+	}
+	return (n);
+}
