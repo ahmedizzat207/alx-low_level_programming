3ab6f24b293f31811cb6a749e6c7dd2cf4ca1fc8
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-01-05 03:24:32 -0800

    Task2:Add: 2-strncpy.c file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copys a string to another one
+ * @dest: the string to be past the copyed string
+ * @src: the string to be copyed
+ * @n: the number to characters to be copyed
+ *
+ * Return: the resulted (dest) string
+ */
+
+char *_strncpy (char *dest, char *src, int n)
+{
+	int counter;
+
+	counter = 0;
+	while (counter < n)
+	{
+		*(dest + counter) = *(src + counter);
+		if (*(src + counter) == '\0')
+			break;
+		counter++;
+	}
+	return (dest);
+}
