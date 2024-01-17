0bb97dca381cc897b4b4862b77e57cd3bc086b29
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-01-17 03:39:06 -0800

    Task6:Fix: the 1 situation

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,11 +12,20 @@ int is_prime_number(int n)
 	return (prime_number(n, 2));
 }
 
+/**
+ * prime_number - check if the number is prime number or not
+ * @n: the number to be checked
+ * @i: the dividing number
+ *
+ * Return: always return (1) if the number is prime and (0) if the number is
+ * composite
+ */
+
 int prime_number(int n, int i)
 {
 	if ((n % i) == 0)
 		return (0);
-	else if ((i >= (n / 2)) && ((n % i) == 0))
-		return (-1);
+	else if ((i >= (n / 2)))
+		return (1);
 	return (prime_number(n, i + 1));
 }
