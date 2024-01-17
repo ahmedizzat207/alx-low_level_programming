f3f0001f8d8dbb04ed6072f955c5e1a328ed1028
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-01-17 03:43:55 -0800

    Task6:Fix: fix the negative and 1 situation

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -23,7 +23,9 @@ int is_prime_number(int n)
 
 int prime_number(int n, int i)
 {
-	if ((n % i) == 0)
+	if (n < 1)
+		return (0);
+	else if ((n % i) == 0)
 		return (0);
 	else if ((i >= (n / 2)))
 		return (1);
