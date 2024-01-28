53cc6e47cd4bd7995a8012e90ae2003c78647605
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-01-28 01:42:36 -0800

    Task2:Fix: the null string stuation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * str_concat - The function concatenates two given strings
@@ -18,14 +19,22 @@ char *str_concat(char *s1, char *s2)
 	unsigned long int s1len;
 	unsigned long int s2len;
 
-	s1len = strlen((const char *)s1);
-	s2len = strlen((const char *)s2);
+	i = 0;
+	i2 = 0;
+	if (s1 == NULL)
+		s1len = 0;
+	else
+		s1len = strlen((const char *)s1);
+	if (s2 == NULL)
+		s2len = 0;
+	else
+		s2len = strlen((const char *)s2);
 	ptr = malloc(sizeof(char) * (s1len + s2len + 1));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; (unsigned long)i < s1len; i++)
+	for (; (unsigned long)i < s1len && s1 != NULL; i++)
 		ptr[i] = s1[i];
-	for (i2 = 0; (unsigned long)i2 < s2len; i2++)
+	for (; (unsigned long)i2 < s2len && s2 != NULL; i2++)
 	{
 		ptr[i] = s2[i2];
 		i++;
