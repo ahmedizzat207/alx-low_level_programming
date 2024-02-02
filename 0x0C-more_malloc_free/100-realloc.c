3dd43eb68fec3111305d860583e92040a677afe2
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-02-02 06:20:31 -0800

    Task4:Add: 100-main.c, 100-realloc.c, 100-realloc files

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+void *cpy_backup(void *ptr, unsigned int new_size);
+
+/**
+ * _realloc - The funciton reallocates a memory block using malloc and free
+ * @ptr: a void pointer to the memory previously allocated
+ * @old_size: the size, in bytes of the allocated space for ptr
+ * @new_size: the new size, int bytes of the new memory block
+ *
+ * Return: a void pointer to the new allocated memory block, or NULL
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	int diff;
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	diff = new_size - old_size;
+	if (diff == 0)
+		return (ptr);
+	ptr = cpy_backup(ptr, new_size);
+	return (ptr);
+}
+
+/**
+ * cpy_backup - The function copies all the possible content of old pointer ptr
+ * to new pointer backup
+ * @ptr: a void pointer to the memory previously allocated
+ * @new_size: the new size, in bytes of the new memory block
+ *
+ * Return: a void pointer to the allocated new memory block, or NULL if the
+ * function fails
+ */
+
+void *cpy_backup(void *ptr, unsigned int new_size)
+{
+	void *backup;
+
+	backup = malloc(new_size);
+	if (backup == NULL)
+		return (NULL);
+	strncpy(backup, ptr, new_size);
+	free(ptr);
+	return (backup);
+}
