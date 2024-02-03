65f2b17eb9ed1871c8cd5636e9e796e582bc8d24
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-02-03 03:35:53 -0800

    Task3:Fix: betty style, double inclusion

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
 #ifndef FUNCTION_LIKE_MACRO
-#define FNUCTION_LIKE_MACRO
-
-#define ABS(x) ((x) < (0) ? -x : x)
-
+#define FUNCTION_LIKE_MACRO
+#define ABS(x) ((x) < (0) ? (-x) : (x))
 #endif /* #ifndef FUNCTION_LIKE_MARCO */
