4ba49067f7cf2a8ce5f11abbfa7fc170db17f659
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-02-12 08:43:26 -0800

    Task0:Fix: betty documetation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG
 #define DOG
 
+/**
+ * dog - a structure for the dog information
+ * @name: the name of the dog
+ * @owner: the owner of the dog
+ * @age: the age of the dog
+ */
+
 struct dog
 {
 	char *name, *owner;
