01071beecdf119d2766e24db4bb9b77ac6476a49
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-02-15 07:28:34 -0800

    Task1:Add: 1-print_numbers.c file and modify variadic_functions.h library

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+
+/**
+ * print_numbers - The function prints numbers given as arguments, followed by
+ * a new line.
+ * @separator: a pointer to a char (string) that specify the string that should
+ * be printed between numbers
+ * @n: an unsigned integer that specify the number of given arguments
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arguments;
+
+	if (n != 0)
+	{
+		va_start(arguments, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(arguments, int));
+			if (separator != NULL && (i + 1) != n)
+				printf("%s", separator);
+		}
+		_putchar('\n');
+	}
+}
