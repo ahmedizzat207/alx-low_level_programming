37cfe39657597c55e847b2ff9fac5d1d39f00b03
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-02-16 06:41:38 -0800

    Task3:Add: 3-print_all.c file and variadic_functions.h library

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+
+/**
+ * print_all - The function prints anything
+ * @format: a pointer to a character (string) that contains all the arguments
+ * types respectively
+ *
+ * Description: The function should print all four types (c:char, i:integer,
+ * f:float, s: char *) that given by order in the format string to specify
+ * the types order and number of arguments by default.
+ */
+
+void print_all(const char * const format, ...)
+{
+	struct identifiers checker[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
+	int formatcount;
+	int checkercount;
+	int sepcheck;
+	va_list arguments;
+	va_list *argptr;
+
+	va_start(arguments, format);
+	argptr = &arguments;
+	formatcount = 0;
+	sepcheck = 0;
+	while (format[formatcount] != '\0' && format != NULL)
+	{
+		checkercount = 0;
+		while (checker[checkercount].character != '\0')
+		{
+
+			if (format[formatcount] == checker[checkercount].character)
+			{
+				if (sepcheck != 0)
+					printf(", ");
+				checker[checkercount].charfunc(argptr);
+				sepcheck++;
+			}
+			checkercount++;
+		}
+		formatcount++;
+	}
+	printf("\n");
+}
+
+/**
+ * print_char - The function print a character from the calling variadic
+ * function
+ * @argptr: a pointer of type va_list to the arguments list passed to the
+ * calling variadic function
+ */
+
+void print_char(va_list *argptr)
+{
+	printf("%c", va_arg(*argptr, int));
+}
+
+/**
+ * print_int - The function print an integer from the calling variadic
+ * function
+ * @argptr: a pointer of type va_list to the argument list passed to the
+ * calling variadic function
+ */
+
+void print_int(va_list *argptr)
+{
+	printf("%d", va_arg(*argptr, int));
+}
+
+/**
+ * print_float - The function print a float from the calling variadic function
+ * @argptr: a pointer of type va_list to the argument list passed to the
+ * calling variadic function
+ */
+
+void print_float(va_list *argptr)
+{
+	printf("%f", va_arg(*argptr, double));
+}
+
+/**
+ * print_string - The function prints a string from the calling variadic
+ * function
+ * @argptr: a pointer of type va_list to the argument list passed to the
+ * calling variadic function
+ */
+
+void print_string(va_list *argptr)
+{
+	printf("%s", va_arg(*argptr, char *));
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,38 @@
 #ifndef VARIADICFUNCTIONS
 #define VARIADICFUNCTIONS
 
+#include <stdarg.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
+void print_char(va_list *argptr);
+void print_int(va_list *argptr);
+void print_float(va_list *argptr);
+void print_string(va_list *argptr);
+
+
+
+/**
+ * struct identifiers - The structure store the types characters and related
+ * functions
+ * @character: a character to compare the types from the given string on the
+ * function
+ * @charfunc: a pointer to a function that have a void return type and
+ * a pointer to a va_list type as it's argument the function prints the
+ * argument of the specified type
+ *
+ * Description: The structure works as a storage of types and specific
+ * functions to print that specified type
+ */
+
+struct identifiers
+{
+	char character;
+	void (*charfunc)(va_list *argptr);
+};
 
 #endif /* #ifndef VARIADICFUNCTIONS */
