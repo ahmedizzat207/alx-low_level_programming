24b56346f47c9d10c5750ffa66d1fcec2c9b6aa6
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-02-25 07:39:10 -0800

    Task9:Fix: some checkes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,21 +19,39 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp2;
 	unsigned int count;
 
-	if (!head && !*head)
-		return (NULL);
 	temp = *head;
 	temp2 = *head;
-	for (count = 1; temp && count < idx; count++)
+	for (count = 0; temp && count + 1 < idx; count++)
 	{
 		temp = temp->next;
 		temp2 = temp2->next;
 	}
-	temp2 = temp2->next;
-	temp->next = malloc(sizeof(**head));
-	if (!temp->next)
-		return (NULL);
-	temp = temp->next;
-	temp->n = n;
-	temp->next = temp2;
+	if (idx == 0)
+	{
+		temp = malloc(sizeof(**head));
+		if (!temp)
+			return (NULL);
+		temp->n = n;
+		if (head != NULL && *head != NULL)
+		{
+			temp->next = *head;
+			*head = temp;
+		}
+		else
+		{
+			temp->next = NULL;
+			*head = temp;
+		}
+	}
+	else if (temp)
+	{
+		temp2 = temp2->next;
+		temp->next = malloc(sizeof(**head));
+		if (!temp->next)
+			return (NULL);
+		temp = temp->next;
+		temp->n = n;
+		temp->next = temp2;
+	}
 	return (temp);
 }
