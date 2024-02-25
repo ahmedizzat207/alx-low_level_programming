c92b7a0e4c6d2282f48e5ece5af8e17735a933af
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-02-25 03:01:40 -0800

    Task5:Add: 5-free_listint2.c file and modified version of lists.h library

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * free_listint2 - The function frees all the elements of listint_t list
+ * @head: a pointer to a pointer to the first element of listint_t list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+	for (; temp;)
+	{
+		temp = temp->next;
+		free(*head);
+		*head = temp;
+	}
+}
