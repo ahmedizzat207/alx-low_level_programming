05309b835ca3f62c7f3536ea1a73579efe19d927
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-02-27 06:44:44 -0800

    Task4:Fix: betty coding style and index overflow

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,7 +7,7 @@
  * @n: a pointer to unsigned long integer contain the main value to set on it
  * the index bit to 0
  * @index: an unsigned integer contain the index of the bit to set to 0
- * 
+ *
  * Return: 1 in success, or -1 if an error occurred
  */
 
@@ -16,7 +16,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	unsigned long int power;
 	unsigned int count;
 
-	if (index >= (sizeof(*n) * 8) && !n)
+	if ((index >= (sizeof(*n) * 8)) && !n)
 		return (-1);
 	power = 1;
 	for (count = 0; count < index; count++)
