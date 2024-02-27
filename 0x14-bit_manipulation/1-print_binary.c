266df272a264d5ece52cf799ab72f7eb2db458f8
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-02-27 04:04:30 -0800

    Task1:Add: 1-print_binary.c file and modified version of main.h library

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - The function prints the binary representation of a number
+ * @n: an unsigned long integer that contain the number to print in binary
+ *
+ * Description: The function shouldn't use arrays nor malloc not % and / 
+ * operators and it had no return value
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int check;
+	int count;
+
+	check = n;
+	for (count = 0; check; count++)
+	{
+		check = (check >> 1);
+	}
+	if (!n)
+		_putchar('0');
+	for (check = n, count--; count >= 0; count--)
+	{
+		check = n;
+		check = (((check >> count) & 1) + 48);
+		_putchar((char)check);
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 unsigned int _pow(unsigned int basic, unsigned int power);
+void print_binary(unsigned long int n);
 
 
 #endif /* #ifndef MAIN_H */
