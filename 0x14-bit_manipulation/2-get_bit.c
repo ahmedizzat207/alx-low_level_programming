6140d8f78e479b804ac22ab7d8532434e1e850f3
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-02-27 05:54:47 -0800

    Task2:Add: 2-get_bit.c file and modified version of main.h library

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+/**
+ * get_bit - The function returns the value of a bit at a given index
+ * @n: an usigned long integer contain the whole number to grab a bit from it
+ * @index: an unsigned integer represent the position to grab the value from n
+ *
+ * Return: an integer of the number of the bit grabed either 1 or 0, or -1 if
+ * an error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return ((n & _pow(2, index)) >> index);
+}
+
+/**
+ * _pow - The function calculates the exponent result of two number
+ * @basic: an unsigned integer represent the basic number of the exponent
+ * @power: an unsigned integer represent the power number of the exponent
+ *
+ * Return: an unsigned integer contain the result of the exponent
+ */
+unsigned int _pow(unsigned int basic, unsigned int power)
+{
+	unsigned int result, count;
+
+	result = 1;
+	for (count = 0; count < power; count++)
+		result *= basic;
+	return (result);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,7 @@ int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 unsigned int _pow(unsigned int basic, unsigned int power);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 
 #endif /* #ifndef MAIN_H */
