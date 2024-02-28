d2a9273f7d467cb3bafbb80a81966a941eb8a1b8
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-02-28 09:34:50 -0800

    Task0:Add: 1-create_file.c and modified version of main.h library

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+/**
+ * create_file - The function creates a file
+ * @filename: a constent pointer to character (string) that conatain the file
+ * name to create
+ * @text_content: a pointer to character (string) that should be included in
+ * the new created file
+ *
+ * Return: 1 on success, or -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int charcount, file, failnum;
+
+	if (!filename)
+		return (-1);
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY,  S_IRUSR | S_IWUSR);
+	if (file < 0)
+		return (-1);
+	charcount = strlen(text_content);
+	if (charcount)
+	{
+		failnum = write(file, text_content, charcount);
+		if (failnum != charcount)
+			return (-1);
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+}
