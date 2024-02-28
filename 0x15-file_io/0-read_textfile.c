638813dc1f38858a0de3455b536b4672dd58ff8c
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-02-28 08:05:47 -0800

    Task0:Add: README.md and 0-read_textfile.c files and main.h library

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - The function reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: a constent pointer to character (string) that contain filename
+ * path to be read and printed to the standard output
+ * @letters: a number of type size_t that represent the letters which should be
+ * printed
+ *
+ * Return: the number of letters it read or printed by write
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *text;
+	ssize_t num;
+	int file;
+
+	if (!filename)
+		return (0);
+	file = open(filename, O_CREAT | O_RDONLY);
+	text = malloc(sizeof(*text) * letters);
+	if (!text)
+		return (0);
+	num = write(STDOUT_FILENO, text, read(file, text, letters));
+	if (num <= (ssize_t)strlen(text))
+		return (num);
+	else
+		return (0);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+size_t read_textfile(const char *filename, size_t letters);
+
+
+#endif /* #ifndef MAIN_H */
