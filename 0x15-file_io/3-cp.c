b0d4171a935713a4b02fc65000982c2e4c21afd1
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-02-29 03:19:49 -0800

    Task3:Fix: remove the cp argument required before

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,21 +24,21 @@ int main(int argc, char **argv)
 	char buffer[READ];
 	int filefrom, fileto, charcount, errorfrom, errorto;
 
-	if (argc != 4)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fileto = open(argv[3], O_CREAT | O_TRUNC | O_WRONLY, 00600 | 00060 | 00006);
-	filefrom = open(argv[2], O_RDONLY);
+	fileto = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 00600 | 00060 | 00006);
+	filefrom = open(argv[1], O_RDONLY);
 	if (filefrom < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	else if (fileto < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[3]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	for (charcount = READ; charcount == READ;)
