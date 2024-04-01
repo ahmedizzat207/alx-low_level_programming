ec9766f8254a30cd7d98c23fdcaccd500a45ee59
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-04-01 08:22:03 -0700

    Task7:Add: 7-insert_dnodeint.c , 2-add_dnodeint.c and 3-add_dnodeint_end.c and a modified version of the lists.h library

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - The function inserts a new node at a given
+ * position
+ * @h: a pointer to a pointer containing the address of the first node in the
+ * dlistint_t list
+ * @idx: an unsigned integer containing the index where to add the new node in
+ * @n: an integer containing the value assigned to the new added node
+ *
+ * Return: a pointer to the newly added node, or NULL if it failed or the index
+ * is not found
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *prev, *next, *new;
+	unsigned int count;
+
+	new = malloc(sizeof(*new));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	prev = *h;
+	next = *h;
+	for (count = 0; next && count < idx; count++)
+	{
+		prev = next;
+		next = prev->next;
+	}
+	if (count == idx)
+	{
+		new->next = next;
+		new->prev = prev;
+		next->prev = new;
+		if (idx == 0)
+			new->prev = NULL;
+		else
+			prev->next = new;
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (new);
+}
